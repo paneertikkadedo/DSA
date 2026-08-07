614f4ba6864437b69afc9b4e9a400b1cd67a65c0
paneertikkadedo <paneertikkadedo@gmail.com>
2026-08-07 12:29:20 +0530

    Time: 1560 ms (7.25%), Space: 55.8 MB (32.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,17 +1,31 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length/2;
-        int answer = 0;
-        HashMap<Integer,Integer> freq = new HashMap<>();
-        for(int num : nums){
-            freq.put(num,freq.getOrDefault(num,0)+1);
-        }
+    //     int n = nums.length/2;
+    //     int answer = 0;
+    //     HashMap<Integer,Integer> freq = new HashMap<>();
+    //     for(int num : nums){
+    //         freq.put(num,freq.getOrDefault(num,0)+1);
+    //     }
 
-        for(int key : freq.keySet()){
-            if(freq.get(key)>n){
-                answer = key;
-            }
+    //     for(int key : freq.keySet()){
+    //         if(freq.get(key)>n){
+    //             answer = key;
+    //         }
+    //     }
+    // return answer;
+
+    // brute force app
+    int n = nums.length;
+    for(int i=0; i<n; i++){
+        int count = 0;
+        for(int j=i; j<n; j++){
+            if(nums[i]==nums[j])
+            count++;
         }
-    return answer;
+        if(count > n/2){
+            return nums[i];
+        }
+    }
+    return -1;
     }
 }
