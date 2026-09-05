733af42cfa0fc14790c1206215668068e6b9cea3
paneertikkadedo <paneertikkadedo@gmail.com>
2026-09-06 02:58:12 +0530

    Time: 0 ms (100%), Space: 43.7 MB (70.89%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,17 +1,43 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        int[][] ans = new int[n][n];
+        // int n = matrix.length;
+        // int[][] ans = new int[n][n];
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         ans[j][n - 1 - i] = matrix[i][j];
+        //     }
+        // }
+
+        // // copy ans back into matrix
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = 0; j < n; j++) {
+        //         matrix[i][j] = ans[i][j];
+        //     }
+        // }
+
+                int n = matrix.length;
+
+        // Transpose
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                ans[j][n - 1 - i] = matrix[i][j];
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-        // copy ans back into matrix
+        // Reverse each row
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                matrix[i][j] = ans[i][j];
+            int left = 0;
+            int right = n - 1;
+
+            while (left < right) {
+                int temp = matrix[i][left];
+                matrix[i][left] = matrix[i][right];
+                matrix[i][right] = temp;
+
+                left++;
+                right--;
             }
         }
     }
